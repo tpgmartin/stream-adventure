22d5cfa1d8d8eb7756ce38bfdaaf29ae67a02037
Tom Martin <tom@tpgmartin.com>
2015-04-10 16:01:59 +0100

    HTTP, websockets exercises

---
+++ http_server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var through = require('through2');
+
+var server = http.createServer(function (req, res) {
+  if (req.method === 'POST') {
+    req.pipe(through(function (buffer, _, next) {
+      this.push(buffer.toString().toUpperCase());
+      next();
+    })).pipe(res);
+  } else {
+    res.end('sfsd');      
+  }
+});
+
+server.listen(process.argv[2]);
